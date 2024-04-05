98ce4d12e7c24d8c8ab4eb97fa428c08b118acff
D.A.J.A.Madhuwantha <arjun.madhuwantha@gmail.com>
2024-04-05 23:42:46 +0530

    Register Allert fix

--- Bank-Insuarance/obj/Debug/net8.0/Bank-Insuarance.AssemblyInfo.cs
+++ Bank-Insuarance/obj/Debug/net8.0/Bank-Insuarance.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bank-Insuarance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e836e2b59ab18f4a75b33839d2ac55052642de34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7112aed7ffd2af12b22019c51deb11f3bd7866a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bank-Insuarance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bank-Insuarance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
