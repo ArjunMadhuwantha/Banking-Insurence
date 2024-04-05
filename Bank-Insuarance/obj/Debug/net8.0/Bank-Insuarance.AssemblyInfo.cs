71044f14cc3e773090f9e343c9d6f1bc5bb828e6
D.A.J.A.Madhuwantha <arjun.madhuwantha@gmail.com>
2024-04-05 23:17:27 +0530

    Register DOB fix

--- Bank-Insuarance/obj/Debug/net8.0/Bank-Insuarance.AssemblyInfo.cs
+++ Bank-Insuarance/obj/Debug/net8.0/Bank-Insuarance.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bank-Insuarance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b579571892e82b6b57930b3220e63c52ab57975b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8b6579b7bdc1c33c055c777e090934827f86560")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bank-Insuarance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bank-Insuarance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
