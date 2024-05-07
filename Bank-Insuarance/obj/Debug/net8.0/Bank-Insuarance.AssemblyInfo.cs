68ac3ea11708d8c11ab3fc3c4ed8de2877f34712
S.D.M.V. Samarathunga <118242094+Maduka-Vinod@users.noreply.github.com>
2024-05-07 12:34:28 +0530

    NIC bug FIX

--- Bank-Insuarance/obj/Debug/net8.0/Bank-Insuarance.AssemblyInfo.cs
+++ Bank-Insuarance/obj/Debug/net8.0/Bank-Insuarance.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bank-Insuarance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c98170050c1a38e9dc52d7a6931b88e320f02d24")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bank-Insuarance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bank-Insuarance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
