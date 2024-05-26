94fffe8d510add0d778f94d3d4871160d622703b
D.A.J.A.Madhuwantha <arjun.madhuwantha@gmail.com>
2024-05-26 14:30:47 +0530

    Fixed Future date Error

--- Bank-Insuarance/obj/Debug/net8.0/Bank-Insuarance.AssemblyInfo.cs
+++ Bank-Insuarance/obj/Debug/net8.0/Bank-Insuarance.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bank-Insuarance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f8dbcf9d86b568f773f684dc38c41c184c80c4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10bfcd0d21594d98b2f32bd90b315d61fc3a7d02")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bank-Insuarance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bank-Insuarance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
