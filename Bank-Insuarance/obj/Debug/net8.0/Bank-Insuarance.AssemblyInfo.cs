0af607dc90b8c0bd2efdb28fd9d0f1d01711451a
D.A.J.A.Madhuwantha <arjun.madhuwantha@gmail.com>
2024-05-27 12:27:56 +0530

    Branch & Zone ID Allert style set

--- Bank-Insuarance/obj/Debug/net8.0/Bank-Insuarance.AssemblyInfo.cs
+++ Bank-Insuarance/obj/Debug/net8.0/Bank-Insuarance.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bank-Insuarance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18a70a065ec7605cc5991c9c6bb14a16264a98ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe13725ff3aaa26af204c6707aaca43d0d58310a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bank-Insuarance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bank-Insuarance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
