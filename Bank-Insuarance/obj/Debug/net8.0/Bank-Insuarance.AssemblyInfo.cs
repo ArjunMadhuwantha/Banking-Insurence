b025fbfec85d7848e0cef31dd0659494009a983c
D.A.J.A.Madhuwantha <arjun.madhuwantha@gmail.com>
2024-07-08 14:15:21 +0530

    Lone date error fix

--- Bank-Insuarance/obj/Debug/net8.0/Bank-Insuarance.AssemblyInfo.cs
+++ Bank-Insuarance/obj/Debug/net8.0/Bank-Insuarance.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bank-Insuarance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62660f800be4b68ca4c20fe6b4d1c6f531c8d299")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67f4e245479ec75ca6b8ba260e243fbbed05698e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bank-Insuarance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bank-Insuarance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
