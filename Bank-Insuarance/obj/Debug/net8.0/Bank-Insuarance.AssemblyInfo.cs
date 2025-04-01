6613177577b869634d52852595cc9d712ec9c2d8
S.D.M.V. Samarathunga <118242094+Maduka-Vinod@users.noreply.github.com>
2025-04-01 11:33:57 +0530

    last update divisarana reg address

--- Bank-Insuarance/obj/Debug/net8.0/Bank-Insuarance.AssemblyInfo.cs
+++ Bank-Insuarance/obj/Debug/net8.0/Bank-Insuarance.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bank-Insuarance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b025fbfec85d7848e0cef31dd0659494009a983c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+511d6d65acc3a371a63bad1824e65782a7c6c027")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bank-Insuarance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bank-Insuarance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
